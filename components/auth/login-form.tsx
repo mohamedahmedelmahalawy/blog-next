76778ee9d2cc8ba39f595eacd3fbb5f5a26eaeb3
mizomico <aroky.me@gmail.com>
2026-04-30 16:47:06 +0300

    login-form & register-form Done

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,3 +1,82 @@
+"use client";
+
+import { useState } from "react";
+import { z } from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "@/components/ui/input";
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+import { Button } from "../ui/button";
+
+const loginSchema = z.object({
+  email: z.string().email({ error: "please enter a valid email value" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long." }),
+});
+
+type LoginFormValues = z.infer<typeof loginSchema>;
+
 export default function LoginForm() {
-  return <div>login-form</div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit = async (data: LoginFormValues) => {
+    setIsLoading(true);
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      {/* EMAIL */}
+      <Controller
+        name="email"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+
+            <Input
+              {...field}
+              id="email"
+              placeholder="Enter your email"
+              aria-invalid={fieldState.invalid}
+            />
+
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      {/* PASSWORD */}
+      <Controller
+        name="password"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+
+            <Input
+              {...field}
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              aria-invalid={fieldState.invalid}
+            />
+
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Button type="submit" className="w-full" disabled={isLoading}>
+        {isLoading ? "Signing in..." : "Sign In"}
+      </Button>
+    </form>
+  );
 }
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,3 +1,121 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { Field, FieldError, FieldLabel } from "../ui/field";
+
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const registerSchema = z
+  .object({
+    name: z.string().min(3, { message: "Name must be 3 characters long." }),
+    email: z.string().email({ error: "Please enter a valid email address!" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be 6 characters long." }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be 6 characters long." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type RegisterFormValues = z.infer<typeof registerSchema>;
 export default function RegisterForm() {
-  return <div>register-form</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const onSubmit = async (data: RegisterFormValues) => {
+    setIsLoading(true);
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      {/* Name */}
+      <Controller
+        name="name"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="name">Name</FieldLabel>
+            <Input
+              {...field}
+              id="name"
+              placeholder="Enter your Name"
+              aria-invalid={fieldState.invalid}
+            />
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      {/* email */}
+      <Controller
+        name="email"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              {...field}
+              id="email"
+              placeholder="Enter your Email"
+              aria-invalid={fieldState.invalid}
+            />
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      {/* password */}
+      <Controller
+        name="password"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input
+              {...field}
+              id="password"
+              placeholder="Enter your Password"
+              aria-invalid={fieldState.invalid}
+            />
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      {/* confirm password */}
+      <Controller
+        name="confirmPassword"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor="confirmPassword">confirmPassword</FieldLabel>
+            <Input
+              {...field}
+              id="confirmPassword"
+              placeholder="Confirm your Password"
+              aria-invalid={fieldState.invalid}
+            />
+            {fieldState.error && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Button type="submit" className="w-full" disabled={isLoading}>
+        {isLoading ? "Creating Account" : "Create Account"}
+      </Button>
+    </form>
+  );
 }
